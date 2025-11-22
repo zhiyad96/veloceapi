e500336e8765c582bf07dc9725c44e6d35bb6150
zhiyad96 <mhdzhiyad9656@gmail.com>
2025-11-22 22:40:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); 
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(router);
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`🚀 JSON Server is running on port ${port}`);
+});
